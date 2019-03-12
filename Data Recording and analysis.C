e294760d4c01e559592809d60a68c5db0cf4e467
abperni <abperni@gmail.com>
2019-03-12 20:47:59 +0545

    Initial program

---
+++ Data Recording and analysis.C
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+struct info{
+	char name[30];
+	int roll_no;
+	struct markofstu{
+		int maths;
+		int dl;
+		int iit;
+		int c;
+		int phy;
+	}marks;
+}inf[1000];
+void main(){
+	struct info data,log;
+	int n,i,j;
+	clrscr();
+	printf("Enter total number of students");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		clrscr();
+		printf("\nEnter name of student");
+		scanf("%s",&inf[i].name);
+		printf("\nEnter your roll number:");
+		scanf("%d",&inf[i].roll_no);
+		printf("\nEnter your marks in Maths");
+		scanf("%d",&inf[i].marks.maths);
+		printf("\nEnter your marks in Digital Logic");
+		scanf("%d",&inf[i].marks.dl);
+		printf("\nEnter your marks in IIT");
+		scanf("%d",&inf[i].marks.iit);
+		printf("\nEnter your marks in C Programming");
+		scanf("%d",&inf[i].marks.c);
+		printf("\nEnter your makrs in Physics");
+		scanf("%d",&inf[i].marks.phy);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=(i+1);j<n;j++)
+		{
+			if(inf[i].roll_no>inf[j].roll_no)
+			{
+				log=inf[i];
+				inf[i]=inf[j];
+				inf[j]=log;
+			}
+		}
+	}
+	clrscr();
+	printf("\n\nRoll No\tName of Student\tMaths\tDL\tIIT\tC Prog\tPhysics");
+	for(i=0;i<n;i++)
+	{
+		printf("\n%d\t%s\t\t%d\t%d\t%d\t%d\t%d",inf[i].roll_no,inf[i].name,inf[i].marks.maths,inf[i].marks.dl,inf[i].marks.iit,inf[i].marks.c,inf[i].marks.phy);
+	}
+	getch();
+}
